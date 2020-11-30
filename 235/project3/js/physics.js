e148b6786c73dbf189c40178ccf69b290278ef99
dek6613 <dek6613@rit.edu>
2020-11-30 18:30:14 -0500

    Created rectangle class with intersection detection

--- 235/project3/js/physics.js
+++ 235/project3/js/physics.js
@@ -7,6 +7,7 @@ class Vector
     {
         this.x = x;
         this.y = y;
+        Object.seal(this);
     }
 
     // Returns the magnitude of the vector
@@ -16,13 +17,13 @@ class Vector
     }
 
     // Returns the vector produced by adding this to the given vector
-    add(other = new Vector())
+    add(other = Vector())
     {
         return Vector(this.x + other.x, this.y + other.y);
     }
 
     // Returns the vector produced by subtracting the given vector from this
-    subtract(other = new Vector())
+    subtract(other = Vector())
     {
         return Vector(this.x - other.x, this.y - other.y);
     }
@@ -38,5 +39,40 @@ class Vector
     {
         let mag = this.magnitude();
         return this.scale(1 / mag);
+    }
+}
+
+// Represents a rectangle defined by the top-left corner and a width/height
+class Rectangle
+{
+    constructor (position = Vector(), width = 0, height = 0)
+    {
+        this.position = position;
+        this.width = width;
+        this.height = height;
+        Object.seal(this);
+    }
+
+    // Uses AABB collision detection to determine whether two rectangles are intersecting
+    intersects (other = Rectangle())
+    {
+        return this.x < other.x + other.width
+        && this.x + this.width > other.x
+        && this.y > other.y - other.height
+        && this.y - this.height < other.y;
+    }
+
+    // Returns the overlapping area of two rectangles
+    overlap (other = Rectangle())
+    {
+        let olWidth = Math.max(this.x, other.x) - Math.min(this.x + this.width, other.x + other.width);
+        let olHeight = Math.min(this.y, other.y) - Math.max(this.y - this.height, other.y - other.height);
+
+        if (olWidth <= 0 || olHeight <= 0) { return Rectangle(); }
+
+        let olX = Math.max(this.x, other.x);
+        let olY = Math.min(this.y, other.y);
+
+        return Rectangle(Vector(olX, olY), olWidth, olHeight);
     }
 }
